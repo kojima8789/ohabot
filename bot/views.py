9f074e5a31e13d5746229e4cd78180666282a357
小島祐太 <oha3641@icloud.com>
2019-11-13 20:40:36 +0900

    commit

--- bot/views.py
+++ bot/views.py
@@ -47,7 +47,7 @@ def handle_message(event):
         line_bot_api.reply_message(
               event.reply_token,
 
-              TextSendMessage(text=weather),
+              TextSendMessage(text=weather)
               # TextSendMessage(text="+ 都市=", data["name"]),
               # TextSendMessage(text="| 天気=", data["weather"][0]["description"]),
               # TextSendMessage(text="| 最低気温=", k2c(data["main"]["temp_min"])),
