ff92cdc6c0628019b30be4bf4564543de24557ad
小島祐太 <oha3641@icloud.com>
2019-11-13 16:55:55 +0900

    commit

--- bot/views.py
+++ bot/views.py
@@ -47,7 +47,7 @@ def handle_message(event):
       line_bot_api.reply_message(
           event.reply_token,
           [
-          TextSendMessage(text=msg)
+          TextSendMessage(msg)
           ]
     )
   else:
