f8a85cedbf80335c92b929397bf1e225afb8ef85
小島祐太 <oha3641@icloud.com>
2019-11-15 10:23:21 +0900

    commit

--- bot/views.py
+++ bot/views.py
@@ -44,19 +44,12 @@ def handle_message(event):
         ]
         )
     elif 'トーキョー' in text:
-        data = we.get_weather_from_api(d)
+        d = we.get_weather_from_api()
         line_bot_api.reply_message(
         event.reply_token,
 
-        TextSendMessage(text=data)
-        # TextSendMessage(text="+ 都市=",data["name"])
-              # TextSendMessage(text="| 天気=", data["weather"][0]["description"]),
-              # TextSendMessage(text="| 最低気温=", k2c(data["main"]["temp_min"])),
-              # TextSendMessage(text="| 最高気温=", k2c(data["main"]["temp_max"])),
-              # TextSendMessage(text="| 湿度=", data["main"]["humidity"]),
-              # TextSendMessage(text="| 気圧=", data["wind"]["deg"]),
-              # TextSendMessage(text="| 風速度=", data["wind"]["speed"]),
-              # TextSendMessage(text="")
+        TextSendMessage(text=d)
+
 
         )
     else:
