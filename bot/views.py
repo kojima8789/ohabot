e40697df1feedb90746227417b651d67388b644f
小島祐太 <oha3641@icloud.com>
2019-11-15 23:38:35 +0900

    commit

--- bot/views.py
+++ bot/views.py
@@ -44,12 +44,12 @@ def handle_message(event):
         ]
         )
     if 'トーキョー' in text:
-        data = we.get_weather_from_api()
+        msg = we.get_weather_from_api()
         line_bot_api.reply_message(
         event.reply_token,
 
 
-        TextSendMessage(text=str(data))
+        TextSendMessage(text=msg)
         # TextSendMessage(text="| 天気=", data["weather"][0]["description"]),
         # TextSendMessage(text="| 最低気温=", k2c(data["main"]["temp_min"])),
         # TextSendMessage(text="| 最高気温=", k2c(data["main"]["temp_max"])),
--- bot/weathermap.py
+++ bot/weathermap.py
@@ -13,6 +13,8 @@ def get_weather_from_api():
     # 温度変換(ケルビン→摂氏)
     k2c = lambda k: k - 273.15
 
+    msg = ""
+
     # 各都市の温度を取得する
     for name in cities:
         # APIのURLを得る
@@ -23,15 +25,15 @@ def get_weather_from_api():
         data = json.loads(r.text)
 
         # 結果を出力
-        d = ("+ 都市=", format(data)["name"])
-        # print("| 天気=", data["weather"][0]["description"])
-        # print("| 最低気温=", k2c(data["main"]["temp_min"]))
-        # print("| 最高気温=", k2c(data["main"]["temp_max"]))
-        # print("| 湿度=", data["main"]["humidity"])
-        # print("| 気圧=", data["wind"]["deg"])
-        # print("| 風速度=", data["wind"]["speed"])
-        # print("")
+        msg += "都市:" + data["name"] + "\n"
+        msg += " 天気:" + data["weather"][0]["description"] + "\n"
+        msg += " 最低気温=" + str(k2c(data["main"]["temp_min"])) + "\n"
+        msg += " 最高気温=" + str(k2c(data["main"]["temp_max"])) + "\n"
+        msg += " 湿度=" + str(data["main"]["humidity"]) + "\n"
+        msg += " 気圧=" + str(data["main"]["pressure"]) + "\n"
+        msg += " 風向き=" + str(data["wind"]["deg"]) + "\n"
+        msg += " 風速度=" + str(data["wind"]["speed"]) + "\n\n"
 
-        return d
+
 
 
